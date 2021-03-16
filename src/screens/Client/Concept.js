db5e4f49bd0e9b9469f6d7a54d6abd24640ed6bb
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2021-03-16 16:21:42 +0100

    concept

--- src/screens/Client/Concept.js
+++ src/screens/Client/Concept.js
@@ -1,10 +1,49 @@
 import React from 'react'
 
-
 function Concept() {
   return (
-    <div>
-      <h2>Hello World, By NASS RAICHOU</h2>
+    <div className='concept'>
+      <h2>Un concept avec de nombreux avantages</h2>
+      <div className='concept-block'>
+        <img className='img-produits' src='images/Annexe1.jpg' alt='maison' />
+        <div className='concept-right'>
+          <p>
+            Iam in altera philosophiae parte. quae est quaerendi ac disserendi,
+            quae logikh dicitur, iste vester plane, ut mihi quidem videtur,
+            inermis ac nudus est. tollit definitiones, nihil de dividendo ac
+          </p>
+        </div>
+      </div>
+      <div className='concept-block'>
+        <div id='first-cr' className='concept-right'>
+          <p>
+            Iam in altera philosophiae parte. quae est quaerendi ac disserendi,
+            quae logikh dicitur, iste vester plane, ut mihi quidem videtur,
+            inermis ac nudus est. tollit definitiones, nihil de dividendo ac
+          </p>
+        </div>
+        <img className='img-produits' src='images/Annexe5.jpg' alt='maison' />
+      </div>
+      <div className='concept-block'>
+        <img className='img-produits' src='images/Annexe6.jpg' alt='maison' />
+        <div className='concept-right'>
+          <p>
+            Iam in altera philosophiae parte. quae est quaerendi ac disserendi,
+            quae logikh dicitur, iste vester plane, ut mihi quidem videtur,
+            inermis ac nudus est. tollit definitiones, nihil de dividendo ac
+          </p>
+        </div>
+      </div>
+      <div className='concept-block'>
+        <div id='last-cr' className='concept-right'>
+          <p>
+            Iam in altera philosophiae parte. quae est quaerendi ac disserendi,
+            quae logikh dicitur, iste vester plane, ut mihi quidem videtur,
+            inermis ac nudus est. tollit definitiones, nihil de dividendo ac
+          </p>
+        </div>
+        <img className='img-produits' src='images/Annexe1.jpg' alt='maison' />
+      </div>
     </div>
   )
 }
