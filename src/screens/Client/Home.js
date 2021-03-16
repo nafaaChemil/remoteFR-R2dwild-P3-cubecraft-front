786bb40eeeb95700bc85cbb8c80e1e3246fad4d7
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2021-03-16 12:33:14 +0100

    homepage done

--- src/screens/Client/Home.js
+++ src/screens/Client/Home.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Slider from 'react-slick'
 import { Link } from 'react-router-dom'
-import './styles/home.scss'
 
 function Home() {
   return (
@@ -11,24 +10,90 @@ function Home() {
         style={{
           backgroundImage: `url("images/Annexe1.jpg")`
         }}
-      ></div>
+      >
+        <div className='home-hero-carousel'>
+          <h1 className='home-title'>Une solution</h1>
+          <div className='carousel-text'>
+            <p>Ecologique</p>
+          </div>
+        </div>
+      </div>
       <section id='home-concept'>
-        <h2></h2>
-        <div className='home-concept-image'></div>
-        <Link to=''>Notre concept</Link>>
+        <h2>
+          HIGH CUBE ECO DESIGN, SPÉCIALISTE DE LA CONSTRUCTION EN CONTENEUR
+          MARITIME
+        </h2>
+        <div className='home-concept-image'>
+          <img
+            className='thumbnail-home-concept'
+            src='images/Annexe3.jpg'
+            alt='a def'
+          />
+          <img
+            className='thumbnail-home-concept'
+            src='images/Annexe3.jpg'
+            alt='a def'
+          />
+        </div>
+        <Link className='buttonHome' to=''>
+          Notre concept
+        </Link>
       </section>
       <section id='home-produits'>
-        <img className='img-produits' src='images/Annexe1.jpg' alt='maison' />
-        <div className='produits-right'>
-          <h2>Professionnels</h2>
-          <p>
-            Iam in altera philosophiae parte. quae est quaerendi ac disserendi,
-            quae logikh dicitur, iste vester plane, ut mihi quidem videtur,
-            inermis ac nudus est. tollit definitiones, nihil de dividendo ac
-          </p>
+        <div className='produits-pro'>
+          <img className='img-produits' src='images/Annexe1.jpg' alt='maison' />
+          <div className='produits-right'>
+            <h2>Professionnels</h2>
+            <p>
+              Iam in altera philosophiae parte. quae est quaerendi ac
+              disserendi, quae logikh dicitur, iste vester plane, ut mihi quidem
+              videtur, inermis ac nudus est. tollit definitiones, nihil de
+              dividendo ac
+            </p>
+            <button className='buttonHome' type='button'>
+              Découvrez nos solutions
+            </button>
+          </div>
+        </div>
+        <div className='produits-part'>
+          <div className='produits-right'>
+            <h2>Particuliers</h2>
+            <p>
+              Iam in altera philosophiae parte. quae est quaerendi ac
+              disserendi, quae logikh dicitur, iste vester plane, ut mihi quidem
+              videtur, inermis ac nudus est. tollit definitiones, nihil de
+              dividendo ac
+            </p>
+            <button className='buttonHome' type='button'>
+              Découvrez nos solutions
+            </button>
+          </div>
+          <img className='img-produits' src='images/Annexe2.jpg' alt='maison' />
         </div>
       </section>
-      <section id='home-actualite'></section>
+      <section id='home-actualite'>
+        <h2>Notre actualités</h2>
+        <div className='container-thumbnail-actualite'>
+          <img
+            className='thumbnails-actualite'
+            src='images/Annexe4.jpg'
+            alt='actu'
+          />
+          <img
+            className='thumbnails-actualite'
+            src='images/Annexe4.jpg'
+            alt='actu'
+          />
+          <img
+            className='thumbnails-actualite'
+            src='images/Annexe4.jpg'
+            alt='actu'
+          />
+        </div>
+        <Link className='buttonHome' to=''>
+          Toute notre actualités
+        </Link>
+      </section>
     </div>
   )
 }
