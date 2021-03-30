f6f166d440feb8b6752edbc0bab42d931c823d02
Alexandre-Rosier <alex.rosier1405@gmail.com>
2021-03-30 10:14:55 +0200

    upload-file

---
+++ src/screens/Admin/AdminPhotos.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+
+import ButtonAdd from '../../components/Admin/ButtonAdd'
+
+export default function AdminPhotos() {
+  const [datas, setDatas] = useState([''])
+  const [affiched, setAffiched] = useState(true)
+  const history = useHistory()
+
+  function AddPhoto() {
+    history.push(`/admin/photos/add`)
+  }
+  const deletePhoto = id => {
+    axios.delete(`http://localhost:4242/photos/${id}`, {}).then(res => {
+      setAffiched(!affiched)
+    })
+  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const resq = await axios.get('http://localhost:4242/photos')
+      setDatas(resq.data)
+    }
+    fetchData()
+  }, [affiched])
+
+  return (
+    <>
+      <section id='admin'>
+        <h1>Photo</h1>
+        <div>
+          <ButtonAdd name='Ajouter une photo' handleClickAdd={AddPhoto} />
+        </div>
+        <div>
+          {datas.map((data, index) => (
+            <>
+              <img key={index} src={data.Name} />
+              <button onClick={() => deletePhoto(data.Id)}>suppr</button>
+            </>
+          ))}
+        </div>
+      </section>
+    </>
+  )
+}
---
+++ src/screens/Admin/AdminPhotosAdd.js
@@ -0,0 +1,60 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+
+export default function AdminPhotosAdd() {
+  const [myImage, setMyImage] = useState([])
+  const [valid, setValid] = useState(false)
+
+  let history = useHistory()
+
+  const handleAdd = e => {
+    setMyImage(oldArray => [...oldArray, e.target.files])
+  }
+
+  const handleSubmit = () => {
+    myImage.map(img =>
+      axios
+        .post(`http://localhost:4242/photos`, {
+          Name: `images/${img[0].name.replace(/ /g, '_')}`
+        })
+        .then(res => {
+          setValid(!valid)
+        })
+    )
+    history.push('/admin/photos')
+  }
+
+
+
+  function comeBack() {
+    history.push('/admin/photos')
+  }
+
+  return (
+    <div className='container-image'>
+      <div>
+        <label> image : </label>
+        <input type='file' onChange={handleAdd} />
+      </div>
+      <div className='block-select-img'>
+        <ul>
+          {myImage.length === 0 ? (
+            <li>Aucune photo</li>
+          ) : (
+            myImage.map((img, i) => (
+              <li key={i}>{img[0].name.replace(/ /g, '_')}</li>
+            ))
+          )}
+        </ul>
+      </div>
+      <button
+        onClick={handleSubmit}
+        style={{ display: `${valid ? 'none' : 'block'}` }}
+      >
+        Envoyer
+      </button>
+      <button onClick={comeBack}>Retour</button>
+    </div>
+  )
+}
