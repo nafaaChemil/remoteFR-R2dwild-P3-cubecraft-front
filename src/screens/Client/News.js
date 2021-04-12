cc169e0e41942b8d88b596a0f20804156d83daf8
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2021-04-12 10:18:31 +0200

    data

--- src/screens/Client/News.js
+++ src/screens/Client/News.js
@@ -1,30 +1,29 @@
-import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import ActuEncart from '../../components/Client/ActuEncart'
 import '../../components/Style/elements/actuEncart.scss'
 
 export default function News() {
+  const [datas, setDatas] = useState([''])
+  useEffect(() => {
+    const fetchData = async () => {
+      const resq = await axios.get(`http://localhost:4242/news`)
+      setDatas(resq.data)
+    }
+    fetchData()
+  }, [])
   return (
     <section id='actu'>
       <div className='responsive'>
-        <ActuEncart
-          imageEncart='images/Annexe5.jpg'
-          titleH3='Le Monde2'
-          text='
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-          sodales leo eu libero luctus finibus. Aliquam id eleifend felis. 
-          Pellentesque eget maximus ipsum. Ut hendrerit, turpis vitae lobortis 
-          fermentum, mi lectus aliquam tortor, eu convallis tortor lectus eu 
-          neque. Cras id orci tortor. Vestibulum laoreet malesuada nulla, 
-          eget mollis purus egestas ut. Integer bibendum, justo eu fringilla 
-          elementum, ex magna vestibulum ipsum, vel interdum ligula est vel 
-          eros. Praesent accumsan, urna non sollicitudin porta, orci nisl 
-          interdum elit, id gravida dolor mauris ut nisl. Aliquam lacinia, 
-          lectus sit amet porta iaculis, nisi elit tincidunt purus, nec 
-          suscipit velit lacus in nibh. Interdum et malesuada fames ac ante 
-          ipsum primis in faucibus. In libero odio, consequat nec sodales ac, 
-          feugiat non velit.'
-          link='Lien'
-        />
+        {datas.map((data, index) => (
+          <ActuEncart
+            key={index}
+            imageEncart={'images/Annexe5.jpg'}
+            titleH3={data.Title}
+            text={data.Text}
+            link={data.Link}
+          />
+        ))}
         <ActuEncart
           imageEncart='images/Annexe4.jpg'
           titleH3='Journal'
