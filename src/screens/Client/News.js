f5ff7a021373d487956e0849546198be4e162d51
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2021-04-15 16:45:09 +0200

    catch error actuality and footer

--- src/screens/Client/News.js
+++ src/screens/Client/News.js
@@ -1,20 +1,23 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import ActuEncart from '../../components/Client/ActuEncart'
+import { History } from 'react-router-dom'
 
 export default function News() {
   const [datas, setDatas] = useState([''])
+  const [linkOk, setLinkOk] = useState([])
+
   useEffect(() => {
     const fetchData = async () => {
       const resq = await axios.get(`http://localhost:4242/news`)
       setDatas(resq.data)
-      console.log(resq.data)
     }
     fetchData()
   }, [])
+
   return (
     <section id='actu'>
-      <h2 className='uCenter'>Actualités.</h2>
+      <h2 className='uCenter'>Actualités</h2>
       <div className='responsive'>
         {datas.map((data, index) => (
           <ActuEncart
@@ -23,6 +26,10 @@ export default function News() {
             titleH3={data.Title}
             text={data.Text}
             link={data.Link}
+            linktext={
+              // data.Link ? data.Link.includes('https://') ? data.Link > 25 ? data.Link.substring(0,25): data.Link.replace('https://www.', "") : null
+              data.Link ? data.Link.length > 26 ? data.Link.substring(0, 30).replace("https://www." ,"") + "..." : data.Link.replace("https://www." ,"") : data.Link
+            }
           />
         ))}
       </div>
