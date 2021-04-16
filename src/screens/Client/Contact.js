f8f1f93026d21a79a85ace9e20fdd8225c3fdd4f
Alexandre-Rosier <alex.rosier1405@gmail.com>
2021-04-16 11:01:45 +0200

    resolve conflict

--- src/screens/Client/Contact.js
+++ src/screens/Client/Contact.js
@@ -10,93 +10,95 @@ const Contact = () => {
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
   const [valid, setValid] = useState(false)
-  const [consent, setConsent] = useState(true)
-  
+
   const handleSubmit = event => {
     event.preventDefault()
-    emailjs.send(
-      'service_9ba3ccb',
-      'template_dnukio6',
-      {
-        firstName,
-        lastName,
-        phone,
-        email,
-        message
-      },
-      'user_eyvbTv2muWvgFT0taOlAM'
-    )
-    .then(() => {
-      setFirstName('')
-      setLastName('')
-      setPhone('')
-      setEmail('')
-      setMessage('')
-      setValid(!valid)
-    })
-    .catch(() => (document.querySelector('form-message').innerHTML =
-    "une erreur s'est produite.")
-    )
+    emailjs
+      .send(
+        'service_9ba3ccb',
+        'template_dnukio6',
+        {
+          firstName,
+          lastName,
+          phone,
+          email,
+          message
+        },
+        'user_eyvbTv2muWvgFT0taOlAM'
+      )
+      .then(() => {
+        setFirstName('')
+        setLastName('')
+        setPhone('')
+        setEmail('')
+        setMessage('')
+        setValid(!valid)
+      })
+      .catch(
+        () =>
+          (document.querySelector('form-message').innerHTML =
+            "une erreur s'est produite.")
+      )
   }
 
-  function reset() {
-    document.getElementById('').reset()
-  }
-  
   return (
-    <div className='Contact'>
-      <div>
-        <h1 className='title-form'>Plus d'infos ?</h1>
-      </div>
-      
-      <form onSubmit={handleSubmit} className='contact-form'>
+    <>
+      <div className='Contact'>
+        <div>
+          <h1 className='title-form'>Plus d'infos ?</h1>
+        </div>
 
-        <div className='form-group'>
-          <input placeholder='Nom / Raison sociale *'
-            type='text'
-            value={lastName}
-            className='lastName'
-            name='last_name'
-            required 
-            onChange={event => setLastName(event.target.value)}
+        <form onSubmit={handleSubmit} className='contact-form'>
+          <div className='form-group'>
+            <input
+              placeholder='Nom / Raison sociale *'
+              type='text'
+              value={lastName}
+              className='lastName'
+              name='last_name'
+              required
+              onChange={event => setLastName(event.target.value)}
             />
-        </div>
-        <hr />
+          </div>
+          <hr />
 
-        <div className='form-group'>
-          <input placeholder='Prénom'
-            type='text'
-            value={firstName}
-            name='user_firstname'
-            className='firstName'
-            onChange={event => setFirstName(event.target.value)}
-          />
-        </div>
-        <hr />
+          <div className='form-group'>
+            <input
+              placeholder='Prénom'
+              type='text'
+              value={firstName}
+              name='user_firstname'
+              className='firstName'
+              onChange={event => setFirstName(event.target.value)}
+            />
+          </div>
+          <hr />
 
-        <div className="form-group">
-            <input placeholder="Email *"
-              type="email"
-              name="user_email"
+          <div className='form-group'>
+            <input
+              placeholder='Email *'
+              type='email'
+              name='user_email'
               value={email}
-              className="email"
+              className='email'
               required
               onChange={event => setEmail(event.target.value)}
-               />
-        </div>
-        <hr />
+            />
+          </div>
+          <hr />
 
-        <div className="form-group">
-            <input type="tel"
-              pattern="[0-9]{10}"
-              maxLength="10"
-              placeholder="Téléphone"
+          <div className='form-group'>
+            <input
+              type='tel'
+              pattern='[0-9]{10}'
+              maxLength='10'
+              placeholder='Téléphone'
               value={phone}
-              className="phone"
+              className='phone'
               onChange={event => setPhone(event.target.value)}
-               />
-        </div>
-        <hr />
+            />
+          </div>
+          <hr />
 
           <div className='form-group'>
             <textarea
@@ -110,63 +112,19 @@ const Contact = () => {
             />
           </div>
           <hr />
-<<<<<<< HEAD
-            {valid && <p className="p-valid">Message envoyé</p>}
-        <div className="form-group">
-          <input className="input-form" 
-            type="submit"
-            value='Envoyer'
-            className="form-btn"
-            />
-        </div>
-        <div className="consent">
-      <input className="input-consent" type="checkbox"
-        required
-        /*consent={consent}
-        onChange={() => setConsent(!consent)}*/
-      />
-       <p className="p-consent">En soumettant ce formulaire, j'accepte que les informations saisies soient exploitées dans le cadre de la relation commerciale qui pourrait en découler.</p>
-     </div>
-      </form>
-    </div>
-    
-    );
-    }
-    
-export default Contact;
-          
-          
-      
-    
- 
-
-
-=======
-
+          {valid && <p className='p-valid'>Message envoyé</p>}
           <div className='form-group'>
-            <input
-              className='input-form'
-              type='submit'
-              value='Envoyer'
-              className='form-btn'
-            ></input>
-            {valid && <p>Message envoyé</p>}
+            <input className='form-btn' type='submit' value='Envoyer' />
+          </div>
+          <div className='consent'>
+            <input className='input-consent' type='checkbox' required />
+            <p className='p-consent'>
+              En soumettant ce formulaire, j'accepte que les informations
+              saisies soient exploitées dans le cadre de la relation commerciale
+              qui pourrait en découler.
+            </p>
           </div>
         </form>
-        <div className='consent'>
-          <input
-            className='input-consent'
-            type='checkbox'
-            required
-            consent={consent}
-            onChange={() => setConsent(!consent)}
-          />
-          <p className='p-consent'>
-            En soumettant ce formulaire, j'accepte que les informations saisies
-            soient exploitées dans le cadre de la relation commerciale qui
-            pourrait en découler.
-          </p>
-        </div>
       </div>
       <MapLeaflet />
     </>
@@ -174,4 +132,3 @@ export default Contact;
 }
 
 export default Contact
->>>>>>> 3ec35ed87f9dbee4cf8b2b50f1ef19e31027bd71
